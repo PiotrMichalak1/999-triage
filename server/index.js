19231eccb0d9209d17395d8dfc3e8a3192b3909b
PiotrM <128514914+PiotrMichalak1@users.noreply.github.com>
2024-10-16 11:59:42 +0200

    Triage initial commit linter, prettier

--- server/index.js
+++ server/index.js
@@ -1,37 +1,47 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
+require("dotenv").config();
+const express = require("express");
+const https = require("https");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
-app.get('/api/get-speech-token', async (req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    const speechKey = process.env.SPEECH_KEY;
-    const speechRegion = process.env.SPEECH_REGION;
+app.get("/api/get-speech-token", async (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  const speechKey = process.env.REACT_APP_TRIAGE_SPEECH_KEY;
+  const speechRegion = process.env.SPEECH_REGION;
+  if (
+    speechKey === "paste-your-speech-key-here" ||
+    speechRegion === "paste-your-speech-region-here"
+  ) {
+    res
+      .status(400)
+      .send("You forgot to add your speech key or region to the .env file.");
+  } else {
+    const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
-    if (speechKey === 'paste-your-speech-key-here' || speechRegion === 'paste-your-speech-region-here') {
-        res.status(400).send('You forgot to add your speech key or region to the .env file.');
-    } else {
-        const headers = { 
-            headers: {
-                'Ocp-Apim-Subscription-Key': speechKey,
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-
-        try {
-            const tokenResponse = await axios.post(`https://${speechRegion}.api.cognitive.microsoft.com/sts/v1.0/issueToken`, null, headers);
-            res.send({ token: tokenResponse.data, region: speechRegion });
-        } catch (err) {
-            res.status(401).send('There was an error authorizing your speech key.');
-        }
+    try {
+      const tokenResponse = await axios.post(
+        "https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken",
+        null,
+        {
+          headers: {
+            "Ocp-Apim-Subscription-Key": speechKey,
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          httpsAgent,
+        },
+      );
+      res.send({ token: tokenResponse.data, region: speechRegion });
+    } catch (err) {
+      res.status(401).send("There was an error authorizing your speech key.");
     }
+  }
 });
 
 app.listen(3001, () =>
-    console.log('Express server is running on localhost:3001')
+  console.log("Express server is running on localhost:3001"),
 );
