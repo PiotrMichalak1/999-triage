bb9f1c326150f19e32c29a4c7d7edd9d4352ddc5
PiotrM <128514914+PiotrMichalak1@users.noreply.github.com>
2024-10-18 16:25:22 +0200

    Cleaned app from server and token utils. Removed unused functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { Container } from "reactstrap";
-import { getTokenOrRefresh } from "./token_util";
 import "./custom.css";
-import { ResultReason } from "microsoft-cognitiveservices-speech-sdk";
 
 const speechsdk = require("microsoft-cognitiveservices-speech-sdk");
 
@@ -13,46 +11,13 @@ export default function App() {
   const [assessmentText, setAssessmentText] = useState(
     "WAITING FOR ASSESSMENT...",
   );
-  const [player, updatePlayer] = useState({ p: undefined, muted: false });
-
-  async function sttFromMic() {
-    //const tokenObj = await getTokenOrRefresh();
-    const speechConfig = speechsdk.SpeechConfig.fromSubscription(
-      process.env.REACT_APP_TRIAGE_SPEECH_KEY,
-      process.env.REACT_APP_TRIAGE_SPEECH_REAGION,
-    );
-
-    const autoDetectSourceLanguageConfig =
-      speechsdk.AutoDetectSourceLanguageConfig.fromLanguages([
-        "en-US",
-        "ru-RU",
-        "pl-PL",
-        "ja-JP",
-      ]);
-    const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
-    const recognizer = speechsdk.SpeechRecognizer.FromConfig(
-      speechConfig,
-      autoDetectSourceLanguageConfig,
-      audioConfig,
-    );
-
-    setTranslationText("speak into your microphone...");
-    recognizer.recognizeOnceAsync((result) => {
-      if (result.reason === ResultReason.RecognizedSpeech) {
-        setTranslationText(`RECOGNIZED: Text=${result.text}`);
-      } else {
-        setTranslationText(
-          "ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.",
-        );
-      }
-    });
-  }
 
   async function sttFromMicWithTranslationToEnglish() {
-    const speechTranslationConfig = speechsdk.SpeechTranslationConfig.fromSubscription(
-      process.env.REACT_APP_TRIAGE_SPEECH_KEY,
-      process.env.REACT_APP_TRIAGE_SPEECH_REGION,
-    );
+    const speechTranslationConfig =
+      speechsdk.SpeechTranslationConfig.fromSubscription(
+        process.env.REACT_APP_TRIAGE_SPEECH_KEY,
+        process.env.REACT_APP_TRIAGE_SPEECH_REGION,
+      );
     speechTranslationConfig.speechRecognitionLanguage = "en-GB"; //need to specify for proper TranslationRecognizer initialization
     speechTranslationConfig.addTargetLanguage("en");
     const autoDetectSourceLanguageConfig =
@@ -85,96 +50,6 @@ export default function App() {
     );
   }
 
-  async function textToSpeech() {
-    const tokenObj = await getTokenOrRefresh();
-    const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(
-      tokenObj.authToken,
-      tokenObj.region,
-    );
-    const myPlayer = new speechsdk.SpeakerAudioDestination();
-    updatePlayer((p) => {
-      p.p = myPlayer;
-      return p;
-    });
-    const audioConfig = speechsdk.AudioConfig.fromSpeakerOutput(player.p);
-
-    let synthesizer = new speechsdk.SpeechSynthesizer(
-      speechConfig,
-      audioConfig,
-    );
-
-    const textToSpeak =
-      "This is an example of speech synthesis for a long passage of text. Pressing the mute button should pause/resume the audio output.";
-    setTranslationText(`speaking text: ${textToSpeak}...`);
-    synthesizer.speakTextAsync(
-      textToSpeak,
-      (result) => {
-        let text;
-        if (
-          result.reason === speechsdk.ResultReason.SynthesizingAudioCompleted
-        ) {
-          text = `synthesis finished for "${textToSpeak}".\n`;
-        } else if (result.reason === speechsdk.ResultReason.Canceled) {
-          text = `synthesis failed. Error detail: ${result.errorDetails}.\n`;
-        }
-        synthesizer.close();
-        synthesizer = undefined;
-        setTranslationText(text);
-      },
-      function (err) {
-        setTranslationText(`Error: ${err}.\n`);
-
-        synthesizer.close();
-        synthesizer = undefined;
-      },
-    );
-  }
-
-  async function handleMute() {
-    updatePlayer((p) => {
-      if (!p.muted) {
-        p.p.pause();
-        return { p: p.p, muted: true };
-      } else {
-        p.p.resume();
-        return { p: p.p, muted: false };
-      }
-    });
-  }
-
-  async function fileChange(event) {
-    const audioFile = event.target.files[0];
-    console.log(audioFile);
-    const fileInfo = audioFile.name + ` size=${audioFile.size} bytes `;
-
-    setTranslationText(fileInfo);
-
-    const tokenObj = await getTokenOrRefresh();
-    const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(
-      tokenObj.authToken,
-      tokenObj.region,
-    );
-    speechConfig.speechRecognitionLanguage = "en-US";
-
-    const audioConfig = speechsdk.AudioConfig.fromWavFileInput(audioFile);
-    const recognizer = new speechsdk.SpeechRecognizer(
-      speechConfig,
-      audioConfig,
-    );
-
-    recognizer.recognizeOnceAsync((result) => {
-      let text;
-      if (result.reason === ResultReason.RecognizedSpeech) {
-        text = `RECOGNIZED: Text=${result.text}`;
-      } else {
-        text =
-          "ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.";
-      }
-
-      setTranslationText(fileInfo + text);
-    });
-  }
-
   return (
     <Container className="app-container vh-100">
       <h1 className="display-4 mb-3">999-triage</h1>
@@ -186,32 +61,6 @@ export default function App() {
             onClick={() => sttFromMicWithTranslationToEnglish()}
           ></i>
           Translate speech to English text from your mic.
-          <div className="mt-2">
-            <label htmlFor="audio-file">
-              <i className="fas fa-file-audio fa-lg mr-2"></i>
-            </label>
-            <input
-              type="file"
-              id="audio-file"
-              onChange={(e) => fileChange(e)}
-              style={{ display: "none" }}
-            />
-            Convert speech to text from an audio file.
-          </div>
-          <div className="mt-2">
-            <i
-              className="fas fa-volume-up fa-lg mr-2"
-              onClick={() => textToSpeech()}
-            ></i>
-            Convert text to speech.
-          </div>
-          <div className="mt-2">
-            <i
-              className="fas fa-volume-mute fa-lg mr-2"
-              onClick={() => handleMute()}
-            ></i>
-            Pause/resume text to speech output.
-          </div>
         </div>
         <div className="col-6">
           <div className="output-display rounded h-50">
